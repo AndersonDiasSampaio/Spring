2885f27ea9c735094033ed8494aa93e69cdc362b
AndersonDiasSampaio <ads@ic.ufal.br>
2021-11-29 10:18:32 -0300

    Primeiro Commit

---
+++ aprendendospring-mvc23/src/main/java/com/spring/curso/AprendendospringMvc23Application.java
@@ -0,0 +1,13 @@
+package com.spring.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendendospringMvc23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprendendospringMvc23Application.class, args);
+	}
+
+}
---
+++ aprendendospring-mvc23/src/test/java/com/spring/curso/AprendendospringMvc23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprendendospringMvc23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
