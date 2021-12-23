bd100f44cf1e04dd3c7f252fb2c6ef28a9dacddd
AndersonDiasSampaio <ads@ic.ufal.br>
2021-12-23 12:01:40 -0300

    create data.sql and cliente table for h2 BD

--- aprendendospring-mvc23/src/main/java/com/spring/curso/AprendendospringMvc23Application.java
+++ aprendendospring-mvc23/src/main/java/com/spring/curso/AprendendospringMvc23Application.java
@@ -1,11 +1,21 @@
 package com.spring.curso;
 
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class AprendendospringMvc23Application {
-
+	
+/*	@Value("${application.dias}")
+	private String Name;
+	
+	@GetMapping("/hello")
+	public String helloWorld() {
+		return this.Name;
+	}*/
 	public static void main(String[] args) {
 		SpringApplication.run(AprendendospringMvc23Application.class, args);
 	}
