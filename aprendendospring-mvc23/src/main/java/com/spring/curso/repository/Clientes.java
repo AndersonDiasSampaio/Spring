f56085404ca5d12fca49dc1d02a0b375262fbff8
AndersonDiasSampaio <ads@ic.ufal.br>
2021-12-24 19:39:48 -0300

    CommandLiner Runer

--- aprendendospring-mvc23/src/main/java/com/spring/curso/repository/Clientes.java
+++ aprendendospring-mvc23/src/main/java/com/spring/curso/repository/Clientes.java
@@ -1,8 +1,20 @@
 package com.spring.curso.repository;
 
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.*;
+
+import com.spring.curso.domain.Cliente;
 
 @Repository
 public class Clientes {
+	@Autowired
+	private JdbcTemplate jdbctemplate;
+	private static String INSERT = "insert into cliente (nome) values (?)";
+	public Cliente  Salvar(Cliente cliente) {
+		
+	jdbctemplate.update(INSERT, new Object[] {cliente.getNome()});
+	return cliente;
+	}
 
 }
