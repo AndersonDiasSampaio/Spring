c971e2b9ca97b8521371bb50d5c4675ed6ea28c4
AndersonDiasSampaio <ads@ic.ufal.br>
2021-12-23 21:13:40 -0300

    create SQL tables

--- aprendendospring-mvc23/src/main/resources/data.sql
+++ aprendendospring-mvc23/src/main/resources/data.sql
@@ -1,3 +1,19 @@
 create table CLIENTE (
 ID integer primary key auto_increment, NOME varchar(100)
  );
+create table PRODUTO (
+ID integer primary key auto_increment, NOME varchar(100), PRECO_UNITARIO numeric(20,2)
+ );
+ create table PEDIDO (
+ID integer primary key auto_increment,
+CLIENTE_ID integer references CLIENTE(ID),
+DATA_PEDIDO timestamp,
+total numeric(20,2)
+
+ );
+ create table ITEM_PEDIDO(
+ ID integer primary key auto_increment, 
+ PEDIDO_ID integer references PEDIDO(ID),
+ PRODUTO_ID integer references PRODUTO(ID),
+ QUANTIDADE integer
+ );
