1e81d9ea2716342d85914e9f3c57e086d5df7cea
AndersonDiasSampaio <ads@ic.ufal.br>
2021-12-24 19:40:09 -0300

    Main LineRunner

--- aprendendospring-mvc23/src/main/java/com/spring/curso/AprendendospringMvc23Application.java
+++ aprendendospring-mvc23/src/main/java/com/spring/curso/AprendendospringMvc23Application.java
@@ -1,10 +1,15 @@
 package com.spring.curso;
 
 import org.springframework.beans.factory.annotation.*;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.curso.domain.Cliente;
+import com.spring.curso.repository.Clientes;
 
 @SpringBootApplication
 public class AprendendospringMvc23Application {
@@ -16,6 +21,17 @@ public class AprendendospringMvc23Application {
 	public String helloWorld() {
 		return this.Name;
 	}*/
+	
+	@Bean
+	public CommandLineRunner init (@Autowired Clientes clientes) {
+		return args ->{
+			Cliente cliente = new Cliente();
+			cliente.setNome("anderson");
+			clientes.Salvar(cliente);
+			
+		};
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AprendendospringMvc23Application.class, args);
 	}
